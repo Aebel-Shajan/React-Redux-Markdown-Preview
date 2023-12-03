261f6e1394c29b4d81daac32a7bbd1f67eb49160
Aebel Shajan <aebelshajan@gmail.com>
2023-12-03 22:34:30 +0000

    Started Front End Dev Libraries Certification project: Markdown Previewer

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const [previewText, setPreviewText] = useState("");
+
+
+
+	return (
+		<div id='container'>
+			<textarea
+				id="text-input"
+				onChange={(e) => { setPreviewText(e.target.value) }}>
+
+			</textarea>
+			<div id="preview-panel">
+				<p id="preview-text">{previewText}</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
